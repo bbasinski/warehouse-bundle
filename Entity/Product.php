591899b97847283fdfd0a6c6821de618d6f4c8ad
Bartosz <basinski.bartosz@gmail.com>
2018-08-01 20:08:52 +0200

    Product entity, bundle version

---
+++ Entity/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Bbasinski\WarehouseBundle\Entity;
+
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $amount;
+}
