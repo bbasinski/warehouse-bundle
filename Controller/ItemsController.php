1bc77256a9e1bbbe62694659f23fed670cc769fd
Bartosz <basinski.bartosz@gmail.com>
2018-08-04 13:46:33 +0200

    Helper json method instead of JsonResponse object returning

--- Controller/ItemsController.php
+++ Controller/ItemsController.php
@@ -4,13 +4,12 @@ namespace Bbasinski\WarehouseBundle\Controller;
 
 use Bbasinski\WarehouseBundle\Repository\ItemRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ItemsController extends Controller
 {
     public function available(ItemRepository $itemRepository)
     {
-        return new JsonResponse(
+        return $this->json(
             [
                 'items' => $itemRepository->findAllAvailableItems()
             ]
@@ -19,7 +18,7 @@ class ItemsController extends Controller
 
     public function unavailable(ItemRepository $itemRepository)
     {
-        return new JsonResponse(
+        return $this->json(
             [
                 'items' => $itemRepository->findAllUnavailableItems()
             ]
@@ -28,7 +27,7 @@ class ItemsController extends Controller
 
     public function amountOver(int $amount, ItemRepository $itemRepository)
     {
-        return new JsonResponse(
+        return $this->json(
             [
                 'items' => $itemRepository->findAllAmountOver($amount)
             ]
